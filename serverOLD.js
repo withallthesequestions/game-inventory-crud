c514bd2b98da7b8a222f6fea4b3d5603b7392366
withallthesequestions <uytpic434516@gmail.com>
2023-04-07 19:24:36 -0400

    Testing new method

--- serverOLD.js
+++ serverOLD.js
@@ -2,10 +2,11 @@
  * Note the `.gitignore` file in the folder (it should contain /node_modules). Ignoring the node_modules mess is best practice when saving to git repo. */
 const express = require("express"); // Imports Express package.
 const server = express(); // express() deploys a server. Assigning it to a variable lets you operate on it.
-const PORT = process.env.PORT || 3000; // Local server port. Note the var name uses LOUDCASE. This is because its value is a magic number value.
+const PORT = process.env.PORT || 4000; // Local server port. Note the var name uses LOUDCASE. This is because its value is a magic number value.
 
-/* Testing something else: const port = process.env.PORT || 3000;
- * Previous value was PORT = 3333
+/* This didn't work: const port = process.env.PORT || 3000;
+ * Updating to 4000
+ * Original value was PORT = 3333
  */
 
 server.use(express.json()); // This is middle-ware. It parses incoming requests which have JSON payloads (eg, POST requests with JSON in their bodies). In order to work, the relevant POST request must have headers which set the content type to JSON. (ie, ‘content-type: application/json’)
@@ -27,8 +28,15 @@ function saveInventory() {
   save(INV_FILE_NAME, chuckInventory);
 }
 
+/* Testing new method below */
+const path = require("path");
 server.get("/toChuck", function (req, res) {
-  res.json(chuckInventory);
+  /*   res.json(chuckInventory); */
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
 });
 
 server.post("/toChuck", function (req, res) {
