bce15104fc86eba414540ee1b7c367bf108fe6e9
withallthesequestions <uytpic434516@gmail.com>
2024-06-04 14:42:40 -0400

    ESM updating

--- jsonUtil.js
+++ jsonUtil.js
@@ -1,18 +1,13 @@
-var fs = require("fs");
+import fs from 'fs';
 
-function load(name) {
-  let text = fs.readFileSync(name, "utf8");
-  let jsonData = JSON.parse(text);
-  return jsonData;
+export function load(name) {
+	let text = fs.readFileSync(name, 'utf8');
+	let jsonData = JSON.parse(text);
+	return jsonData;
 }
 
-function save(name, obj) {
-  let jsonData = JSON.stringify(obj);
-  let result = fs.writeFileSync(name, jsonData);
-  console.log(result);
+export function save(name, obj) {
+	let jsonData = JSON.stringify(obj);
+	let result = fs.writeFileSync(name, jsonData);
+	console.log(result);
 }
-
-module.exports = {
-  load,
-  save,
-};
